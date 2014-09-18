537e61dee598a27f5f13932e49a0f514fdd1dcdf
huangyongfa <huangyongfa3@gmail.com>
2014-09-18 13:08:20 +0800

    的

--- src/com/fnst/base/framework/core/annotation/Column.java
+++ src/com/fnst/base/framework/core/annotation/Column.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
-public @interface Column {
+public @interface Column  {
 	
 	/**
 	 * 字段名称
